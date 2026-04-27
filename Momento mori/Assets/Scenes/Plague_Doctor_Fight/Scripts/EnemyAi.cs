316f6ae91e9d83814c4af4afde295bc85edd744a
Dankestsiren22 <jimmmyshit@gmail.com>
2026-04-27 10:24:37 -0700

    s

--- Momento mori/Assets/Scenes/Plague_Doctor_Fight/Scripts/EnemyAi.cs
+++ Momento mori/Assets/Scenes/Plague_Doctor_Fight/Scripts/EnemyAi.cs
@@ -2,9 +2,34 @@ using UnityEngine;
 using System.Collections;
 public class EnemyAi : MonoBehaviour
 {
-    public int Health;
-    public GameObject[] MoveList;
-    public IEnumerator[] AttackSequences;
+    public int MaxHealth;
+    public int CurrentHealth;
+    public int AttackCoolDown;
 
+    public GameObject[] MoveList;
+    public Sequence[] AttackSequences;
+    public AudioClip[] audioClips;
+
+    public Animator animator;
+
+
+    //----------------------------------------------\\
+    public void Awake()
+    {
+        
+    }
+
+    public void FixedUpdate()
+    {
+        while (AttackCoolDown > 0)
+        {
+            AttackCoolDown--;
+            
+        }
+    }
+    public void Attack()
+    {
+
+    }
  
 }
---
+++ Momento mori/Assets/Scenes/Plague_Doctor_Fight/Scripts/Sequence.cs
@@ -0,0 +1,9 @@
+using System;
+using NUnit.Framework;
+using UnityEngine;
+
+[Serializable]
+public class Sequence
+{
+   // public List[]<string> items;
+}
